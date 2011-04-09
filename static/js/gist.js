198af205ac74b0fd004b06f8bd22c2b4c5713847
felinx <felinx.lee@gmail.com>
2011-04-09 11:38:05 +0800

    Revert "fix forEach IE compatibility"
    This reverts commit 5975fe8e8cd41f6b6220d83fb798d73a4a8d0ca2

--- static/js/gist.js
+++ static/js/gist.js
@@ -1,27 +1,3 @@
-// forEach IE compatibility
-if (!Array.prototype.forEach)
-{
-  Array.prototype.forEach = function(fun /*, thisp */)
-  {
-    "use strict";
-
-    if (this === void 0 || this === null)
-      throw new TypeError();
-
-    var t = Object(this);
-    var len = t.length >>> 0;
-    if (typeof fun !== "function")
-      throw new TypeError();
-
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++)
-    {
-      if (i in t)
-        fun.call(thisp, t[i], i, t);
-    }
-  };
-}
-
 function L() {
    if (window.console && window.console.log)
       console.log.apply(console, arguments);
